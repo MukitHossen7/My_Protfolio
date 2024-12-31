08b90bb59f07609ce2377aa8017e7dd5d1d49276
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-31 14:51:44 +0600

    background images use in my protfolio

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -4,8 +4,13 @@ import Navbar from "../Pages/Share/Navbar/Navbar";
 const MainLayout = () => {
   return (
     <div>
+      {/* <div className="top-0 sticky z-30">
+        <Navbar></Navbar>
+      </div> */}
       <Navbar></Navbar>
-      <Outlet></Outlet>
+      <div className="absolute  z-[-2] min-h-screen w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+        <Outlet></Outlet>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,8 +3,8 @@ const Home = () => {
     <div>
       <div className="flex flex-col items-center">
         {/* Text Below the Logo */}
-        <h1 className="mt-6 text-6xl font-bold tracking-wide text-center">
-          <span className="bg-gradient-to-r from-teal-500 via-[#40BFFC] via-[#D85B8C] via-[#914EE9] to-gray-800 text-transparent bg-clip-text drop-shadow-lg">
+        <h1 className="mt-6 text-6xl font-bold tracking-wide text-center text-white">
+          <span className="bg-gradient-to-r from-purple-600 via-cyan-500 via-sky-600  to-pink-600 text-transparent bg-clip-text drop-shadow-lg">
             Mukit Hossen
           </span>
         </h1>
