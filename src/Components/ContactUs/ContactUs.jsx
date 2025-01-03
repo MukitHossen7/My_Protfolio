088061053d8b6eee75916f934e0dde186e2a1a59
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-03 19:32:09 +0600

    contact page design is complete

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,91 +1,104 @@
 import { motion } from "framer-motion";
 import { CiLocationOn, CiMail } from "react-icons/ci";
+import { MdCall } from "react-icons/md";
+import social from "../../../src/assets/images/social-media-carousel-90.png";
 
 const ContactUs = () => {
   return (
-    <div
-      id="contact"
-      className="py-20 w-10/12 md:w-10/12 lg:w-10/12 xl:w-10/12 2xl:container mx-auto"
-    >
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
+    <div className="pt-8 pb-28 w-10/12 md:w-10/12 lg:w-10/12 xl:w-10/12 2xl:container mx-auto">
+      {/* Heading */}
+      <motion.h1
+        className="text-3xl font-semibold  text-center text-gray-100 mb-10"
+        initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       >
-        <div className="flex flex-col md:flex-row gap-8 items-center ">
-          <div className="w-full lg:w-1/2 ">
-            <motion.div
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              transition={{ duration: 0.6, delay: 0.2 }}
-            >
-              <h2 className="text-2xl md:text-3xl font-semibold text-gray-100 mb-4">
-                Get In Touch
-              </h2>
-              <p className="text-gray-100 font-semibold mb-6 w-full lg:w-2/3">
-                I would love to hear from you! Fill out the form below or email
-                us directly
-              </p>
-              <div className="flex flex-col gap-5">
-                <div className="flex items-center gap-3">
-                  <CiMail className="text-pink-600 text-4xl" />
-                  <div>
-                    <h4 className="text-gray-100 text-lg">Email</h4>
-                    <p className="text-gray-300 text-sm">
-                      hossenmukit7@gmail.com
-                    </p>
-                  </div>
-                </div>
+        Get In Touch
+      </motion.h1>
 
-                <div className="flex items-center gap-3">
-                  <CiLocationOn className="text-4xl text-sky-400" />
-                  <div>
-                    <h4 className="text-gray-100 text-lg">Location</h4>
-                    <p className="text-gray-300 text-sm">Naogaon,Dhamoirhat</p>
-                  </div>
-                </div>
-              </div>
-            </motion.div>
+      {/* Contact Info */}
+
+      <motion.div
+        className="flex  flex-row justify-evenly"
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        {/* Phone */}
+        <div className="group flex flex-col items-center relative">
+          <MdCall className="text-6xl bg-teal-300/20 p-3 rounded-md text-teal-400 group-hover:translate-y-2 transition-all duration-300" />
+          <div className="absolute top-20 opacity-0 group-hover:opacity-100 transition-all duration-300 text-teal-400 text-center">
+            <p className="text-lg font-medium text-gray-100">Call Me</p>
+            <p className="text-sm text-gray-300">+8801326153447</p>
           </div>
+        </div>
 
-          <div className="w-full lg:w-1/2 bg-gray-900 shadow-lg rounded-lg px-6 py-6 ">
-            <form>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <input
-                  type="text"
-                  placeholder="Your Name"
-                  className="p-3 border border-gray-400  rounded-md bg-gray-900 text-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-100"
-                />
-                <input
-                  type="email"
-                  placeholder="Your Email"
-                  className="p-3 border border-gray-400 rounded-md bg-gray-900 text-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-100"
-                />
-              </div>
-              <div>
-                <input
-                  type="email"
-                  placeholder="Your Email"
-                  className="p-3 mt-4  border border-gray-400 rounded-md bg-gray-900 text-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-100 w-full"
-                />
-              </div>
-              <textarea
-                placeholder="Your Message"
-                className="w-full p-3 border border-gray-400 rounded-md mt-4  bg-gray-900 text-gray-100 focus:outline-none focus:ring-1 focus:ring-gray-100"
-                rows="5"
-              ></textarea>
-              <motion.button
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                type="submit"
-                className="w-full bg-gradient-to-r from-indigo-600 via-blue-500 to-teal-500 text-white py-3 rounded-md mt-6 font-semibold"
-              >
-                Send Message
-              </motion.button>
-            </form>
+        {/* Location */}
+        <div className="group flex flex-col items-center relative">
+          <CiLocationOn className="text-6xl bg-sky-300/20 rounded-md p-3 text-sky-400 group-hover:translate-y-2 transition-all duration-300" />
+          <div className="absolute top-20 opacity-0 group-hover:opacity-100 transition-all duration-300 text-gray-100 text-center">
+            <p className="text-lg font-medium text-gray-100">Location</p>
+            <p className="text-sm text-gray-300">Naogaon,Dhamoirhat</p>
+          </div>
+        </div>
+
+        {/* Email */}
+        <div className="group flex flex-col items-center relative">
+          <CiMail className="text-6xl bg-pink-300/20 rounded-md p-3 text-pink-600 group-hover:translate-y-2 transition-all duration-300" />
+          <div className="absolute top-20 opacity-0 group-hover:opacity-100 transition-all duration-300  text-center">
+            <p className="text-lg font-medium text-gray-100">Email</p>
+            <p className="text-sm text-gray-300">hossenmukit7@gmail.com</p>
           </div>
         </div>
       </motion.div>
+
+      {/* Form and Illustration */}
+      <div className="flex gap-10 flex-col md:flex-row items-center justify-center w-full  overflow-hidden mt-14">
+        {/* Form */}
+        <motion.form
+          className="flex-1 flex flex-col space-y-6 p-8 bg-gray-900 rounded-xl shadow-lg "
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="w-full p-4 bg-gray-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-sky-500 placeholder-gray-400 text-white"
+          />
+          <input
+            type="email"
+            placeholder="Your Email"
+            className="w-full p-4 bg-gray-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-sky-500 placeholder-gray-400 text-white"
+          />
+          <textarea
+            placeholder="Your Message"
+            rows="5"
+            className="w-full p-4 bg-gray-800 rounded-lg focus:outline-none focus:ring-2 focus:ring-sky-500 placeholder-gray-400 text-white"
+          ></textarea>
+          <motion.button
+            type="submit"
+            className="w-full bg-gradient-to-r from-indigo-600 via-blue-500 to-teal-500 text-white py-3 rounded-md mt-6 font-semibold transition-all duration-300"
+            whileHover={{ scale: 1.05 }}
+          >
+            Send Message
+          </motion.button>
+        </motion.form>
+
+        {/* Illustration */}
+        <motion.div
+          className="flex-1  "
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.8 }}
+        >
+          <img
+            src={social}
+            alt="Contact Illustration"
+            className="w-full h-auto "
+          />
+        </motion.div>
+      </div>
     </div>
   );
 };
--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -1,12 +1,13 @@
 import { motion } from "framer-motion";
 import { CiLocationOn, CiMail } from "react-icons/ci";
 import { MdCall } from "react-icons/md";
+import social from "../../../src/assets/images/social-media-carousel-90.png";
 const ContactPage = () => {
   return (
-    <div className="min-h-screen  text-white flex flex-col items-center px-6 py-10">
+    <div className="pt-8 pb-28 w-10/12 md:w-10/12 lg:w-10/12 xl:w-10/12 2xl:container mx-auto">
       {/* Heading */}
       <motion.h1
-        className="text-4xl font-extrabold mb-6 text-center"
+        className="text-3xl font-semibold  text-center text-gray-100 mb-10"
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
@@ -17,29 +18,41 @@ const ContactPage = () => {
       {/* Contact Info */}
 
       <motion.div
-        className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-10 mb-10"
+        className="flex  flex-row justify-evenly"
         initial={{ opacity: 0, x: -50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8 }}
       >
         {/* Phone */}
-        <div className="flex items-center space-x-4">
-          <MdCall className="text-4xl text-teal-400" />
+        <div className="group  flex flex-col items-center relative">
+          <MdCall className="text-6xl bg-teal-300/20 p-3 rounded-md text-teal-400 group-hover:translate-y-2 transition-all duration-300" />
+          <div className="absolute top-20 opacity-0 group-hover:opacity-100 transition-all duration-300 text-teal-400 text-center">
+            <p className="text-lg font-medium text-gray-100">Call Me</p>
+            <p className="text-sm text-gray-300">+8801326153447</p>
+          </div>
         </div>
 
         {/* Location */}
-        <div className="flex items-center space-x-4">
-          <CiLocationOn className="text-4xl text-sky-400" />
+        <div className="group flex flex-col items-center relative">
+          <CiLocationOn className="text-6xl bg-sky-300/20 rounded-md p-3 text-sky-400 group-hover:translate-y-2 transition-all duration-300" />
+          <div className="absolute top-20 opacity-0 group-hover:opacity-100 transition-all duration-300 text-gray-100 text-center">
+            <p className="text-lg font-medium text-gray-100">Location</p>
+            <p className="text-sm text-gray-300">Naogaon,Dhamoirhat</p>
+          </div>
         </div>
 
         {/* Email */}
-        <div className="flex items-center space-x-4">
-          <CiMail className="text-pink-600 text-4xl" />
+        <div className="group flex flex-col items-center relative">
+          <CiMail className="text-6xl bg-pink-300/20 rounded-md p-3 text-pink-600 group-hover:translate-y-2 transition-all duration-300" />
+          <div className="absolute top-20 opacity-0 group-hover:opacity-100 transition-all duration-300  text-center">
+            <p className="text-lg font-medium text-gray-100">Email</p>
+            <p className="text-sm text-gray-300">hossenmukit7@gmail.com</p>
+          </div>
         </div>
       </motion.div>
 
       {/* Form and Illustration */}
-      <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-6xl  overflow-hidden">
+      <div className="flex gap-10 flex-col md:flex-row items-center justify-center w-full  overflow-hidden mt-14">
         {/* Form */}
         <motion.form
           className="flex-1 flex flex-col space-y-6 p-8 bg-gray-900 rounded-xl shadow-lg "
@@ -64,7 +77,7 @@ const ContactPage = () => {
           ></textarea>
           <motion.button
             type="submit"
-            className="bg-rose-500 hover:bg-rose-600 text-white py-4 px-8 rounded-lg font-bold text-lg transition-all duration-300"
+            className="w-full bg-gradient-to-r from-indigo-600 via-blue-500 to-teal-500 text-white py-3 rounded-md mt-6 font-semibold transition-all duration-300"
             whileHover={{ scale: 1.05 }}
           >
             Send Message
@@ -73,15 +86,15 @@ const ContactPage = () => {
 
         {/* Illustration */}
         <motion.div
-          className="flex-1 p-6 hidden md:block"
+          className="flex-1  "
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8 }}
         >
           <img
-            src="https://via.placeholder.com/500"
+            src={social}
             alt="Contact Illustration"
-            className="w-full h-auto"
+            className="w-full h-auto "
           />
         </motion.div>
       </div>
