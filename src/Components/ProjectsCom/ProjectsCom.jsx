e8ca980ec0e4d32ca904c35cf8fb3b7c03ec31c7
MukitHossen7 <hossenmukit7@gmail.com>
2025-02-05 17:08:45 +0600

    Update framer-motion to version 11.18.2 and implement fade-in animation effect in ProjectsCom component

--- src/Components/ProjectsCom/ProjectsCom.jsx
+++ src/Components/ProjectsCom/ProjectsCom.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Cards from "../Cards/Cards";
-
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants.js";
 const ProjectsCom = () => {
   const [projects, setProjects] = useState([]);
   useEffect(() => {
@@ -14,17 +15,24 @@ const ProjectsCom = () => {
       id="Project"
       className="w-11/12 md:w-11/12 lg:w-11/12 xl:container mx-auto pt-24"
     >
-      <div className="mb-3 flex justify-center">
-        <div className=" flex flex-col">
-          <h1 className="text-2xl font-bold text-gray-50">
-            All Creative Works.
-          </h1>
-          <span className="block border-b-2 w-20 border-[#3CCF91]"></span>
+      <motion.div
+        variants={fadeIn("left", 0.4)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.8 }}
+      >
+        <div className="mb-3 flex justify-center">
+          <div className=" flex flex-col">
+            <h1 className="text-2xl font-bold text-gray-50">
+              All Creative Works.
+            </h1>
+            <span className="block border-b-2 w-20 border-[#3CCF91]"></span>
+          </div>
         </div>
-      </div>
-      <p className="text-gray-400 mb-8 text-center">
-        Here’s some of my projects that I have worked on.
-      </p>
+        <p className="text-gray-400 mb-8 text-center">
+          Here’s some of my projects that I have worked on.
+        </p>
+      </motion.div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
         {projects?.map((project, index) => (
           <Cards key={index} project={project}></Cards>
